79d831f78069dafc41deecddabc7ae86d188c2fa
Mehrshad <MehrshadB@systemgroup.net>
2023-08-25 15:37:44 +0330

    feature: Add navbar

---
+++ src/components/Shared/Navbar/Navbar.model.ts
@@ -0,0 +1,5 @@
+export interface NavigationModel {
+  name: string | JSX.Element;
+  href: string;
+  current: boolean;
+}
---
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Navbar.css";
+import { NavigationModel } from "./Navbar.model";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+export default function Navabar() {
+  const GithubIcon: () => JSX.Element = () => (
+    <FontAwesomeIcon icon={faGithub} size="xl" />
+  );
+
+  const EnvelopeIcon: () => JSX.Element = () => (
+    <FontAwesomeIcon icon={faEnvelope} size="xl" />
+  );
+
+  const navigation: Array<NavigationModel> = [
+    {
+      name: GithubIcon(),
+      href: "https://github.com/ducksonmoon/cover-generator",
+      current: false,
+    },
+    {
+      name: EnvelopeIcon(),
+      href: "mailto:MehrshadBaqerzadegan@gmail.com",
+      current: false,
+    },
+  ].reverse();
+
+  return (
+    <section className="navigation">
+      <div className="nav-container">
+        <div className="brand">
+          <a href="/">🦴 Cover Generator</a>
+        </div>
+        <nav>
+          <div className="nav-mobile">
+            <a id="nav-toggle" href="#!">
+              <span></span>
+            </a>
+          </div>
+          <ul className="nav-list">
+            {navigation.map((item, index) => (
+              <li key={index}>
+                <a href={item.href}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </section>
+  );
+}
