27d8738864871846746b708e9f53a265684ed394
Mehrshad <MehrshadB@systemgroup.net>
2023-08-26 13:51:58 +0330

    Feature: Add image and download

--- src/components/Generator/Generator.tsx
+++ src/components/Generator/Generator.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+import { useRef, useState } from "react";
+import { Box, Grid, TextField, Typography, Button, Stack } from "@mui/material";
+import { toPng } from "html-to-image";
 
 import SizePicker from "../SizePicker/SizePicker";
 import ColorPicker from "../ColorPicker/ColorPicker";
@@ -11,17 +9,17 @@ import { PlatformModel } from "../SizePicker/Platform.model";
 import { ColorDataModel } from "../ColorPicker/ColorPicker.model";
 import "./Generator.css";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
 export default function Generator() {
   const [size, setSize] = useState({ width: "", height: "" });
   const [backgroundColor, setBackgroundColor] = useState("");
+  const [textInput, setTextInput] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let imageResult = useRef<any>();
+
+  function handleTextInputChange(element) {
+    setTextInput(element.currentTarget.value);
+  }
 
   const setPlatformSize = (platform: PlatformModel) => {
     setSize({
@@ -36,40 +34,123 @@ export default function Generator() {
     );
   };
 
+  const handleImageChange = (event) => {
+    setSelectedImage(event.target.files[0]);
+  };
+
+  const showSelectedImage = (() =>
+    selectedImage && (
+      <img
+        alt="not found"
+        width={"250px"}
+        src={URL.createObjectURL(selectedImage)}
+        className="image--cover"
+      />
+    ))();
+
+  const downloadResult = () => {
+    if (imageResult.current === null) {
+      return;
+    }
+
+    toPng(imageResult.current, { cacheBust: true })
+      .then((dataUrl) => {
+        debugger;
+        const link = document.createElement("a");
+        link.download = "cover.png";
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <main>
+      <Stack
+        direction="row"
+        sx={{
+          justifyContent: "space-between",
+          marginBottom: 2,
+          direction: "rtl",
+        }}
+      >
+        <Button
+          color="secondary"
+          size="medium"
+          variant="contained"
+          onClick={downloadResult}
+        >
+          Download Result
+        </Button>
+      </Stack>
+
       <Grid container spacing={2} columns={16}>
         <Grid item xs={8}>
           <ColorPicker handleColorChange={setColor} />
         </Grid>
         <Grid item xs={8}>
-          <SizePicker handlePlatformSize={setPlatformSize} />
+          <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+            <SizePicker handlePlatformSize={setPlatformSize} />
+            <Button color="primary" size="large" component="label">
+              Upload Photo
+              <input
+                type="file"
+                name="myImage"
+                onChange={handleImageChange}
+                hidden
+              />
+            </Button>
+          </Stack>
         </Grid>
       </Grid>
+      <TextField
+        label="Text"
+        variant="outlined"
+        value={textInput}
+        onChange={handleTextInputChange}
+        sx={{
+          width: "100%",
+          marginTop: 2,
+        }}
+      />
 
       <Box
-        component="span"
         sx={{
-          p: 2,
+          width: "100%",
           border: "1px dashed grey",
           overflow: "scroll",
           boxShadow: 2,
-          m: 1,
+          marginTop: 2,
+          paddingBlock: 3,
           borderRadius: 2,
           textAlign: "center",
-          zoom: "70%",
           display: "flex",
           justifyContent: "center",
         }}
       >
         <Box
+          ref={imageResult}
           id="preview"
           sx={{
             width: size.width,
             height: size.height,
             background: backgroundColor,
           }}
-        ></Box>
+        >
+          <Box sx={{ m: 4 }}>{showSelectedImage}</Box>
+          <Typography
+            sx={{
+              fontWeight: "bolder",
+              fontSize: "xx-large",
+              lineBreak: "anywhere",
+              m: 3,
+            }}
+          >
+            {textInput}
+          </Typography>
+        </Box>
       </Box>
     </main>
   );
